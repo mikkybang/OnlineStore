3e92da538dd91d3f7ddb03f144536d1071cc9f05
Eunovo <eunovo9@gmail.com>
2019-03-11 12:27:29 -0700

    use body-parser middleware to parse json requests

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 // initialize our express app
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 port = 5000;
 
 app.listen(port, () => {

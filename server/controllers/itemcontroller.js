5bee83430feccaf6aa9cca01faef751761c79601
Eunovo <eunovo9@gmail.com>
2019-03-11 16:15:03 -0700

    basic controller setup

---
+++ server/controllers/itemcontroller.js
@@ -0,0 +1,12 @@
+
+exports.create_item = (req, res) => {
+    res.send("Create Item");
+}
+
+exports.get_item = (req, res) => {
+    res.send("Get Item");
+}
+
+exports.get_items_by_category = (req, res) => {
+    res.send("Get Items By Category");
+}

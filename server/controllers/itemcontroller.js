42cf2665594ebee16c5ca5e2acbbd83c2e229a90
Eunovo <eunovo9@gmail.com>
2019-03-13 03:57:06 -0700

    basic request handling

--- server/controllers/itemcontroller.js
+++ server/controllers/itemcontroller.js
@@ -1,12 +1,33 @@
+const itemService = require("../services/itemservice");
 
-exports.create_item = (req, res) => {
-    res.send("Create Item");
+exports.create_item = (req, res, next) => {
+    itemService.create_item("test", "test", 500, "test")
+    .then((item) => {
+        console.log(item);
+        res.json(item);
+    }).catch((err) => {
+        next(err);
+    });
 }
 
-exports.get_item = (req, res) => {
-    res.send("Get Item");
+exports.get_item = (req, res, next) => {
+    itemService.find_item_by_id(req.params.itemId)
+    .then((item) => {
+        console.log(item);
+        res.json(item);
+    })
+    .catch((err) => {
+        next(err);
+    });
 }
 
-exports.get_items_by_category = (req, res) => {
-    res.send("Get Items By Category");
+exports.get_items_by_category = (req, res, next) => {
+    itemService.find_items_by_category(req.params.category)
+    .then((items) => {
+        console.log(items);
+        res.json(items);
+    })
+    .catch((err) => {
+        next(err);
+    });
 }

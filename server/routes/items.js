b0bedc0fe1deba5acff1802e0e78ab2068f57dbd
Eunovo <eunovo9@gmail.com>
2019-03-13 03:53:09 -0700

    added category param to items/category route

--- server/routes/items.js
+++ server/routes/items.js
@@ -5,6 +5,6 @@ const item_controller = require('../controllers/itemcontroller');
 
 router.post('/', item_controller.create_item);
 router.get('/id/:itemId', item_controller.get_item);
-router.get('/category', item_controller.get_items_by_category);
+router.get('/category/:category', item_controller.get_items_by_category);
 
 module.exports = router;

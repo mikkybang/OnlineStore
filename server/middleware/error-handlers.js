fb28965d45712177a103673326a83e279db9fa15
Eunovo <eunovo9@gmail.com>
2019-03-13 17:13:33 -0700

    support for errorhandling across routers

--- server/middleware/error-handlers.js
+++ server/middleware/error-handlers.js
@@ -3,11 +3,11 @@ const ResourceNotFoundError = require("../errors");
 module.exports.resource_not_found = (err, req, res, next) => {
     console.log(err);
     if (err instanceof ResourceNotFoundError) {
-        res.statusCode(404).json({
+        res.status(404).json({
             statusCode: 404,
             message: "error",
             errors: [
-                err.getMessage()
+                err.message
             ]
         });
     } else next(err);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const errorHandlers = require('../middleware/error-handlers');
+
+const resourceNotFoundHandler = errorHandlers.resource_not_found;
 
 module.exports = (app) => {
     const routesFolder = 'routes/';
@@ -13,6 +16,8 @@ module.exports = (app) => {
                 if (!name.endsWith(".js")) return;
                 if (name === "index.js") return;
                 let router = require("./"+name);
+                // define global error handling here
+                router.use(resourceNotFoundHandler);
                 let endpoint = name.substr(0, name.length - 3);
                 app.use(`/${endpoint}`, router);
             });

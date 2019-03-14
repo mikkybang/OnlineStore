e63d39ed72f7253eb76831a8c105b623d9c52b3e
Eunovo <eunovo9@gmail.com>
2019-03-13 17:12:35 -0700

    removed useless lines

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const errorHandlers = require('./middleware/error-handlers');
-// const methodOverride = require('method-override');
 
 // initialize our express app
 const app = express();
@@ -11,15 +9,15 @@ require('./database');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const resourceNotFoundHandler = errorHandlers.resource_not_found;
-
-// app.use(methodOverride());
-app.use(resourceNotFoundHandler);
 
 require('./routes')(app);
+
 
 port = 5000;
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port number ' + port);
 });
+
+// for testing 
+module.exports = app;

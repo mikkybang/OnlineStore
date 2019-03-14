dd83d4490de3f2ea444609f4244ffd95cdfd9050
Eunovo <eunovo9@gmail.com>
2019-03-13 17:11:10 -0700

    added default value for item.description

--- server/models/item.js
+++ server/models/item.js
@@ -4,7 +4,7 @@ let ItemSchema = new mongoose.Schema({
     "name": {type: String, required: true, max: 100},
     "category": {type: mongoose.Types.ObjectId, required: true},
     "price": {type: Number, required: true},
-    "description": {type: String, required: false, max: 1000}
+    "description": {type: String, max: 1000, default: ""}
 });
 
 module.exports = mongoose.model("Item", ItemSchema);

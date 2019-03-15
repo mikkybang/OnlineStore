9d30232e21ef71aecd0e48f2e577c406979fc51f
Eunovo <eunovo9@gmail.com>
2019-03-15 02:32:17 -0700

    added user service

--- server/services/userservice.js
+++ server/services/userservice.js
@@ -1,1 +1,43 @@
 const User = require('../models/user');
+const ResourceNotFoundError = require('../errors/resourcenotfounderror');
+
+const newUser = (username, email, password, role) => {
+    return new User({
+        username,
+        email,
+        password,
+        role
+    });
+}
+
+exports.create_user = (username, email, password) => {
+    return new Promise((resolve, reject) => {
+        let user = newUser(username, email, password);
+        user.save(user, (err, user) => {
+            if (err) reject(err);
+            resolve(user);
+        });
+    });
+}
+
+exports.create_admin_user = (username, email, password) => {
+    return new Promise((resolve, reject) => {
+        let user = newUser(username, email, password, "ADMIN");
+        user.save(user, (err, user) => {
+            if (err) reject(err);
+            resolve(user);
+        });
+    });
+}
+
+exports.find_user_by_id = (userId) => {
+    return new Promise((resolve, reject) => {
+        User.findById(userId)
+        .then((user) => {
+            if (user == null) throw new ResourceNotFoundError(userId);
+            resolve(user);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}

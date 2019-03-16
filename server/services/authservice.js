8baece7178b49ca633792f7d00834bfdb32a0a9f
Eunovo <eunovo9@gmail.com>
2019-03-16 08:24:23 -0700

    added authservice and authservice test

---
+++ server/services/authservice.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const key = require('../key');
+const userservice = require('../services/userservice');
+
+exports.signin = (email, password) => {
+    return new Promise((resolve, reject) => {
+        userservice.find_users({ email })
+        .then((users) => {
+            if (users == null) resolve(null);
+            if (users.length == 0) resolve(null);
+            let user = users[0];
+            user.comparePassword(password, (err, isMatch) => {
+                if (err) throw err;
+                if (isMatch) {
+                    let token = jwt.sign(user._doc, key);
+                    resolve(user);
+                }else resolve(null);
+            });
+        })
+        .catch((err) => {
+            reject(err);
+        });
+    });
+}
---
+++ server/test/services/authservice.js
@@ -0,0 +1,64 @@
+const mock = require('mock-require');
+const expect = require('chai').expect;
+let authservice = require('../../services/authservice');
+
+const User = require('../../models/user');
+
+const mockUser = (mockObj) => {
+    mock('../../models/user', mockObj);
+};
+
+const reRequire = () => {
+    authservice = mock.reRequire('../../services/authservice');
+};
+
+module.exports = (app) => {
+    describe('Test signin', () => {
+        let email = 'eunovo9@gmail.com';
+        let password = 'testpassword';
+        let user = new User({
+            username: 'John',
+            email,
+            password
+        });
+        it('should return the user matching the given login details', (done) => {
+            user.save(user, (err, user) => {
+                authservice.signin(email, password)
+                    .then((result) => {
+                        if (result == null) {
+                            expect.fail('the given login details did not match any user');
+                        }
+                        expect(result).to.have.property('email').to.eq(email);
+                        done();
+                    }).catch((err) => {
+                        expect.fail(err.message);
+                    });
+            });
+        });
+
+        it('should return null for wrong email', (done) => {
+            user.save(user, (err, user) => {
+                authservice.signin('eunovo9@yahoo.com', password)
+                    .then((result) => {
+                        expect(result).to.be.a('null');
+                        done();
+                    }).catch((err) => {
+                        expect.fail(err.message);
+                    });
+            });
+        });
+
+        it('should return null for wrong password', (done) => {
+            user.save(user, (err, user) => {
+                authservice.signin(email, 'wrongpassword')
+                    .then((result) => {
+                        expect(result).to.be.a('null');
+                        done();
+                    }).catch((err) => {
+                        expect.fail(err.message);
+                    });
+            });
+        });
+    });
+
+}

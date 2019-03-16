dbcd75cd2bea152602216296a044516ec8e19093
Eunovo <eunovo9@gmail.com>
2019-03-16 14:09:52 -0700

    added authentication to admin routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// TODO add add authentication middleware 
-// router.use();
+const authMiddleware = require('../middleware/auth-middleware');
+
+router.use(authMiddleware.guardRoute);
 
 const user_controller = require('../controllers/usercontroller');
 const item_controller = require('../controllers/itemcontroller');
 
 router.post('/', user_controller.create_user);
-router.get('/me');
 router.post('/categories/');
 router.post('/items/');
 

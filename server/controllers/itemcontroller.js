4fa5ad2219d26d88dcb443bd7771c8da0df6bd1a
Eunovo <eunovo9@gmail.com>
2019-03-17 07:43:45 -0700

    added create_category and completed create_item functionality

--- server/controllers/itemcontroller.js
+++ server/controllers/itemcontroller.js
@@ -1,41 +1,48 @@
 const itemService = require("../services/itemservice");
 
 exports.create_item = (req, res, next) => {
-    itemService.create_item("test", "test", 500, "test")
-    .then((item) => {
-        console.log(item);
-        res.json(item);
-    }).catch((err) => {
-        next(err);
-    });
+    let { name, category, price, description } = req.body;
+    itemService.create_item(name, category, price, description)
+        .then((item) => {
+            res.json(item);
+        }).catch((err) => {
+            next(err);
+        });
 }
 
 exports.get_all_items = (req, res, next) => {
     itemService.find_all_items()
-    .then((items) => {
-        res.json(items);
-    }).catch((err) => {
-        next(err);
-    });
+        .then((items) => {
+            res.json(items);
+        }).catch((err) => {
+            next(err);
+        });
 }
 
 exports.get_item = (req, res, next) => {
     itemService.find_item_by_id(req.params.itemId)
-    .then((item) => {
-        res.json(item);
-    })
-    .catch((err) => {
-        next(err);
-    });
+        .then((item) => {
+            res.json(item);
+        })
+        .catch((err) => {
+            next(err);
+        });
 }
 
 exports.get_items_by_category = (req, res, next) => {
     itemService.find_items_by_category(req.params.category)
-    .then((items) => {
-        console.log(items);
-        res.json(items);
-    })
-    .catch((err) => {
-        next(err);
-    });
+        .then((items) => {
+            res.json(items);
+        })
+        .catch((err) => {
+            next(err);
+        });
+}
+
+exports.create_category = (req, res, next) => {
+    let { category } = req.body;
+    itemService.create_category(category)
+        .then((saved) => {
+            res.json(saved._doc);
+        }).catch((err) => next(err));
 }
